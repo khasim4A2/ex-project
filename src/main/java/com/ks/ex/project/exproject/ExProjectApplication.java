d7f9dcfe3f21301d7545c9522171866ef68cff84
khasim-git <khasimsoudagar@gmail.com>
2018-12-04 21:44:16 +0530

    inital commit
    initial commit

---
+++ src/main/java/com/ks/ex/project/exproject/ExProjectApplication.java
@@ -0,0 +1,12 @@
+package com.ks.ex.project.exproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExProjectApplication.class, args);
+	}
+}
